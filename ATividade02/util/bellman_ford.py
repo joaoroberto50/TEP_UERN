e1f8fbe153f3e46cb8d1bf52c5d44ebede03558e
Joao <joao@email.com>
2021-10-02 16:53:01 -0300

    Atualizando caminhos Bellman Ford

--- ATividade02/util/bellman_ford.py
+++ ATividade02/util/bellman_ford.py
@@ -2,18 +2,18 @@ from grafo import Grafo
 
 class BellmanFord(object):
 
-    def __init__(self, grafo, ini, fim) -> None:
+    def __init__(self, grafo, ini) -> None:
         self._MAX = 1000000
         self.ini = ini
-        self.fim = fim
         self.grafo = grafo.nos
+        self.camm = []
         self.arestas = {}
         for i in self.grafo:
             self.arestas.update({i:{'u':'', 'peso':self._MAX}})
         self.arestas[self.ini] = {'u':'', 'peso':0}
         
 
-    def relaxa(self, ciclo=False) -> None:
+    def _relaxa(self, ciclo=False) -> None:
         for i in self.arestas:
             for j in self.grafo[i]:
                 x = j[1] + self.arestas[i]['peso']
@@ -28,16 +28,19 @@ class BellmanFord(object):
 
     def run(self) -> None:
         for i in range(0, (len(self.grafo) - 1)):
-            self.relaxa()
+            self._relaxa()
 
 
     def ciclo_negativo(self):
-        return self.relaxa(True)
+        return self._relaxa(True)
 
 
-    def caminho(self):
-        pass
-
+    def caminho(self, fim):
+        self.camm.append(fim)
+        ant = self.arestas[fim]['u']
+        while(ant != ''):
+            self.camm.insert(0, ant)
+            ant = self.arestas[ant]['u']
 
 
 graf = Grafo()
@@ -48,7 +51,9 @@ graf.add_node('x', [['t',-2]])
 graf.add_node('z', [['s',2], ['x',7]])
 # graf.add_node('f', [['d',1], ['e',9]])
 
-bff = BellmanFord(graf, 's', 'f')
+bff = BellmanFord(graf, 's')
 bff.run()
 print(bff.arestas)
 print(bff.ciclo_negativo())
+bff.caminho('z')
+print(bff.camm)
