4953b31583bdf0d965821ae22eb5cc244b22711c
Joao_Roberto <joaoroberto.email.com>
2021-10-21 12:30:56 -0300

    Add Menu

--- ATividade02/ATividade03.py
+++ ATividade02/ATividade03.py
@@ -16,6 +16,9 @@ import copy
 
 HOSPITAIS = ['V39', 'V18', 'V81']
 SAMU = 'V82'
+graf = Grafo('./Dist110_clean.dat')
+djsk = Dijkstra(graf)
+blm = BellmanFord(graf)
 
 
 def sorteia_acidente(vertices):
@@ -25,9 +28,42 @@ def sorteia_acidente(vertices):
             return x
 
 
+def executa(local):
+    caminhos = []
+    pesos = []
+    djsk.caminho(SAMU, local)
+    custo = djsk.ares[local]['peso']
+    print(f'CAMINHO do SAMU ate o local {local}: {djsk.camm} com CUSTO: {custo}')
+    blm.run(local)
+    for i in HOSPITAIS:
+        camm, peso = blm.caminho(i)
+        caminhos.append(camm), pesos.append(peso)
+    print(pesos)
+    print(min(pesos))
+    print(caminhos[pesos.index(min(pesos))])
 
 
+def menu():
+    print(f'Hosptais: {HOSPITAIS}\nSAMU: {SAMU}')
+    while True:
+        x = input(""" 1 - Soertear um local aleatorio\n
+         2 - Digitar um local
+         0 - Sair\n>>>""")
+        if x == '0':
+            break
+        elif x == '1':
+            local = sorteia_acidente([*graf.nos])
+            print(local)
+            executa(local)
+        elif x == '2':
+            local = input("Digite um valor entre V0 e V109, com exceção dos hospitais\n>>>")
+            executa(local)
+        else:
+            print('Digite um valor valido')
 
+
+menu()
+'''
 graf = Grafo('./Dist110_clean.dat')
 djsk = Dijkstra(graf)
 local = sorteia_acidente([*graf.nos])
@@ -49,4 +85,4 @@ print(pesos)
 print(min(pesos))
 print(caminhos[pesos.index(min(pesos))])
 
-
+'''
